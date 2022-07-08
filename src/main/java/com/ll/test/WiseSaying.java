fba794621e4c9b67d38711f0fdcc5cbc6f361670
zelymm <dkdlwkrz@naver.com>
2022-07-08 14:35:11 +0900

    b.txadded JUNIT library

---
+++ src/main/java/com/ll/test/WiseSaying.java
@@ -0,0 +1,23 @@
+package com.ll.test;
+
+public class WiseSaying {
+    private int id;
+    private String content;
+    private String author;
+
+    public WiseSaying(int id, String content, String author) {
+        this.id = id;
+        this.content = content;
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "WiseSaying{" +
+                "id=" + id +
+                ", content='" + content + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
+
+}
