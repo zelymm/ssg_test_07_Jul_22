ec9c72ed33bf8315a3ed71601b3dd73050aef95f
zelymm <dkdlwkrz@naver.com>
2022-07-10 21:19:25 +0900

    added Scanner that accepts pre-entered commands

--- src/test/java/com/ll/test/AppTest.java
+++ src/test/java/com/ll/test/AppTest.java
@@ -1,5 +1,9 @@
 package com.ll.test;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Scanner;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -9,5 +13,24 @@ public class AppTest {
     public void 테스트_실험() {
         int rs = 10 + 20;
         assertEquals(30, rs);
+    }
+
+    @Test
+    public void 테스트_스캐너() {
+        String input = """
+                insert
+                Quote1
+                Author1
+                """.stripIndent();
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        Scanner sc = new Scanner(in);
+
+        String cmd = sc.nextLine().trim();
+        String content = sc.nextLine().trim();
+        String author = sc.nextLine().trim();
+
+        assertEquals("insert", cmd);
+        assertEquals("Quote1", content);
+        assertEquals("Author1", author);
     }
 }
