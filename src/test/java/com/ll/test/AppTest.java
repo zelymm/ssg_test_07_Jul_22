ec6f81d6fd2f55882b16abd5d682f5a9c2a7641c
zelymm <dkdlwkrz@naver.com>
2022-07-10 21:24:18 +0900

    get the outputs as String

--- src/test/java/com/ll/test/AppTest.java
+++ src/test/java/com/ll/test/AppTest.java
@@ -1,7 +1,6 @@
 package com.ll.test;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
+import java.io.*;
 import java.util.Scanner;
 
 import org.junit.jupiter.api.Test;
@@ -16,7 +15,7 @@ public class AppTest {
     }
 
     @Test
-    public void 테스트_스캐너() {
+    public void 문자열을_스캐너의_입력으로_설정() {
         String input = """
                 insert
                 Quote1
@@ -32,5 +31,22 @@ public class AppTest {
         assertEquals("insert", cmd);
         assertEquals("Quote1", content);
         assertEquals("Author1", author);
+    }
+
+    @Test
+    public void 표준출력을_리다이렉션하여_결과를_문자열로_받기() throws IOException {
+        // 표준출력을 리다이렉션
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
+
+        System.out.println("안녕");
+
+        String rs = output.toString().trim();
+
+        // 표준출력을 원상복구
+        System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+        output.close();
+
+        assertEquals("안녕", rs);
     }
 }
