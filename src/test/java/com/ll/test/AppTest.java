b43758fb0cb495dc2047fb35cd6fffc5e142fdf9
zelymm <dkdlwkrz@naver.com>
2022-07-11 15:02:47 +0900

    b.txUrl handler and created Rq class func

--- src/test/java/com/ll/test/AppTest.java
+++ src/test/java/com/ll/test/AppTest.java
@@ -9,6 +9,26 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest {
     @Test
+    public void Rq__getIntParam() {
+        Rq rq = new Rq("삭제?id=1");
+
+        int id = rq.getIntParam("id", 0);
+
+        assertEquals(1, id);
+    }
+
+    @Test
+    public void Rq__getIntParam__2() {
+        Rq rq = new Rq("검색?id=10&no=1");
+
+        int id = rq.getIntParam("id", 0);
+        int no = rq.getIntParam("no", 0);
+
+        assertEquals(10, id);
+        assertEquals(1, no);
+    }
+
+    @Test
     public void 테스트_실험() {
         int rs = 10 + 20;
         assertEquals(30, rs);
