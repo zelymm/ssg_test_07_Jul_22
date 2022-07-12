cba160e903b6a0c88f7519e7edf4ca1e59bb2032
zelymm <dkdlwkrz@naver.com>
2022-07-12 11:21:15 +0900

    Rq Class Refactoring

--- src/main/java/com/ll/test/Rq.java
+++ src/main/java/com/ll/test/Rq.java
@@ -2,19 +2,27 @@ package com.ll.test;
 
 public class Rq {
     String url;
+    String path;
+    String queryStr;
+
     public Rq(String url) {
         this.url = url;
+        String[] urlBits = url.split("\\?", 2);
+        this.path = urlBits[0];
+
+        if (urlBits.length == 2) {
+            this.queryStr = urlBits[1];
+        }
     }
 
     public int getIntParam(String paramName, int defaultValue) {
-        String[] urlBits = url.split("\\?", 2);
-        if ( urlBits.length == 1 ) {
+        if (queryStr == null) {
             return defaultValue;
         }
 
-        urlBits = urlBits[1].split("&");
+        String[] bits = queryStr.split("&");
 
-        for (String urlBit : urlBits) {
+        for (String urlBit : bits) {
             String[] paramNameAndValue = urlBit.split("=", 2);
             String paramName_ = paramNameAndValue[0];
             String paramValue = paramNameAndValue[1];
@@ -25,12 +33,9 @@ public class Rq {
         }
 
         return defaultValue;
-
     }
 
     public String getPath() {
-        String[] urlBits = url.split("\\?", 2);
-
-        return urlBits[0];
+        return path;
     }
 }
